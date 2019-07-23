85d9dc9a9f3d639181cf7ad74f13211211d6ad89
Ubuntu <ubuntu@ip-172-31-45-87.ec2.internal>
2019-07-23 17:04:22 +0000

    views updated

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+# from todo.views import say_hello
+# from todo.views import say_byebye
+from todo.views import get_todo_list, create_an_item, edit_an_item, toggle_status
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # url(r'message$', say_hello),
+    # url(r'poop$', say_byebye)
+    url(r'^$', get_todo_list),
+    url(r'^add$', create_an_item),
+    url(r'^edit/(?P<id>\d+)$', edit_an_item),
+    url(r'^toggle/(?P<id>\d+)$', toggle_status),
 ]
